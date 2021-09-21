a1b5827e00b4f389f0e3980e207da2c9096c8b58
Sreejan <sreejan147@gmail.com>
2021-09-21 17:23:24 +0530

    feat: added app routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Feed from "./pages/Feed";
+import Profile from "./pages/Profile/Profile";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Feed />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/profile" component={Profile} />
+        <Route exact path="/" component={Feed} />
+        {/* <Route path="/"></Route> */}
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import "./Profile.css";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+    </div>
+  );
+};
+
+export default Profile;
