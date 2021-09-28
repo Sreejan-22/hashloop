53a8f31e9d78ed9b073655528a4f9d9189b6ed36
Sreejan <sreejan147@gmail.com>
2021-09-28 21:03:05 +0530

    feat: added PageNotFound page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Feed from "./pages/Feed/Feed";
 import Profile from "./pages/Profile/Profile";
+import PageNotFound from "./pages/PageNotFound";
 import "./App.css";
 
 function App() {
@@ -8,8 +9,10 @@ function App() {
     <Router>
       <Switch>
         <Route path="/profile" component={Profile} />
-        <Route path="/error" component={Profile}></Route>
         <Route exact path="/" component={Feed} />
+        <Route path="*">
+          <PageNotFound />
+        </Route>
       </Switch>
     </Router>
   );
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>This page does not exist!!</h1>
+    </div>
+  );
+};
+
+export default PageNotFound;
