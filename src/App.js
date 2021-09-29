00441e82165a97366a4cfb48678cf388b6326057
Sreejan <sreejan147@gmail.com>
2021-09-29 19:13:43 +0530

    refactor: refactor and rearrangement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Feed from "./pages/Feed/Feed";
 import Profile from "./pages/Profile/Profile";
-import PageNotFound from "./pages/PageNotFound";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 import "./App.css";
 
 function App() {
