959bb1d400fa56d75fc5f50b3cbb23d70be97bb4
Sreejan <sreejan147@gmail.com>
2021-10-05 19:23:14 +0530

    feat: updated auth slice

--- src/slices/auth.slice.js
+++ src/slices/auth.slice.js
@@ -1,5 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// sample user data
+/*
+{
+  name: "John Doe",
+  username: "john",
+  email: "johndoe@gmail.com"
+}
+*/
+
 export const initialState = {
   isAuthenticated: false,
   user: null,
@@ -9,8 +18,10 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    signup: (state, { payload }) => {},
-    login: (state, { payload }) => {},
+    authenticate: (state, { payload }) => {
+      state.isAuthenticated = true;
+      state.user = payload;
+    },
   },
 });
 
@@ -23,11 +34,11 @@ export const authSelector = (state) => state.auth;
 // reducer
 export const authReducer = authSlice.reducer;
 
-// async functions
-const url = "http://localhost:8000";
+// // async functions
+// const url = "http://localhost:8000";
 
-export function signupFn() {
-  return async (dispatch) => {
-    dispatch();
-  };
-}
+// export function signupFn() {
+//   return async (dispatch) => {
+//     dispatch();
+//   };
+// }
