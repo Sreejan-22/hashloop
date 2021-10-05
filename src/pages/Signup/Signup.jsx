97eab88bcdc867b4baec997a1e6516131df81563
Sreejan <sreejan147@gmail.com>
2021-10-05 16:28:00 +0530

    feat: updated Signup page features

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { createTheme, TextField, ThemeProvider } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import amico from "../../assets/amico.svg";
@@ -24,6 +25,57 @@ const useStyles = makeStyles({
 
 const Signup = () => {
   const classes = useStyles();
+  const [name, setName] = useState({
+    text: "",
+    error: false,
+    errorText: null,
+  });
+  const [username, setUsername] = useState({
+    text: "",
+    error: false,
+    errorText: null,
+  });
+  const [email, setEmail] = useState({
+    text: "",
+    error: false,
+    errorText: null,
+  });
+  const [password, setPassword] = useState({
+    text: "",
+    error: false,
+    errorText: null,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setName({ ...name, error: false });
+    setUsername({ ...username, error: false });
+    setEmail({ ...email, error: false });
+    setPassword({ ...password, error: false });
+
+    if (!name.text.length) {
+      setName({ ...name, error: true });
+    }
+    if (!username.text.length) {
+      setUsername({ ...username, error: true });
+    }
+    if (!email.text.length) {
+      setEmail({ ...email, error: true });
+    }
+    if (!password.text.length) {
+      setPassword({ ...password, error: true });
+    }
+
+    if (
+      name.text.length &&
+      username.text.length &&
+      email.text.length &&
+      password.text.length
+    ) {
+      console.log(name.text, username.text, email.text, password.text);
+    }
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -41,6 +93,21 @@ const Signup = () => {
               variant="standard"
               className={classes.textField}
               required
+              onChange={(e) => setName({ ...name, text: e.target.value })}
+              error={name.error}
+              helperText={name.errorText}
+            />
+            <TextField
+              label="Username"
+              type="text"
+              variant="standard"
+              className={classes.textField}
+              required
+              onChange={(e) =>
+                setUsername({ ...username, text: e.target.value })
+              }
+              error={username.error}
+              helperText={username.errorText}
             />
             <TextField
               label="Email"
@@ -48,6 +115,9 @@ const Signup = () => {
               variant="standard"
               className={classes.textField}
               required
+              onChange={(e) => setEmail({ ...email, text: e.target.value })}
+              error={email.error}
+              helperText={email.errorText}
             />
             <TextField
               label="Password"
@@ -55,12 +125,15 @@ const Signup = () => {
               variant="standard"
               className={classes.textField}
               required
+              onChange={(e) =>
+                setPassword({ ...password, text: e.target.value })
+              }
+              error={password.error}
+              helperText={password.errorText}
             />
-            <input
-              type="submit"
-              value="Sign Up"
-              className="signup-submit-btn"
-            />
+            <button className="signup-submit-btn" onClick={handleSubmit}>
+              Sign Up
+            </button>
           </form>
         </div>
       </div>
