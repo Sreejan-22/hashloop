2ad780482111802883963e09f8c9d9a84618e170
Sreejan <sreejan147@gmail.com>
2021-10-05 20:30:53 +0530

    feat: setup Redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { authReducer } from "./slices/auth.slice";
 import "./index.css";
 import App from "./App";
 
+const store = configureStore({
+  reducer: authReducer,
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
