0a4c2a897fa3341bdc05969b8176a046bf60ee45
Sreejan <sreejan147@gmail.com>
2021-10-26 21:24:57 +0530

    refactor: refactor and rearrangement

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { baseUrl } from "../../utils/constants";
+import { notifyError } from "../../utils/notifyToasts";
+import "./Search.css";
+
+const Search = () => {
+  const history = useHistory();
+  const [focus, setFocus] = useState(false);
+  const [searchProfiles, setSearchProfiles] = useState([]);
+
+  const search = async (query) => {
+    try {
+      const res = await fetch(`${baseUrl}/search?name=${query}`);
+      const data = await res.json();
+      if (data.success) {
+        setSearchProfiles(data.profilesFound);
+      } else {
+        notifyError("Failed to fetch search results");
+      }
+    } catch (err) {
+      notifyError("Failed to fetch search results");
+    }
+  };
+
+  let debounceTimeout = 0;
+
+  const handleSearch = async (e) => {
+    const query = e.target.value;
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+
+    if (!query.length) {
+      setSearchProfiles([]);
+      return;
+    }
+
+    debounceTimeout = setTimeout(() => {
+      search(query);
+    }, 300);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search developers"
+        className="search"
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        onChange={handleSearch}
+      />
+      {focus && (
+        <div className="search-results">
+          {searchProfiles.length > 0
+            ? searchProfiles.map((item) => {
+                return (
+                  <div
+                    to={`/profile/${item.username}`}
+                    className="follow-profile search-profile"
+                    key={item._id}
+                    onMouseDown={(e) => {
+                      e.preventDefault();
+                      history.push(`/profile/${item.username}`);
+                    }}
+                  >
+                    <br />
+                    &nbsp;&nbsp;
+                    <img
+                      src={
+                        "pic" in item
+                          ? item.pic
+                          : "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
+                      }
+                      alt=""
+                      className="follow-profile-img"
+                    />
+                    <span>{item.name}</span>
+                  </div>
+                );
+              })
+            : null}
+          {/* <Link to="/profile/sam" className="follow-profile search-profile">
+            <br />
+            &nbsp;&nbsp;
+            <img
+              src="https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
+              alt=""
+              className="follow-profile-img"
+            />
+            <span>Sam Doe</span>
+          </Link> */}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Search;
--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -1,23 +1,20 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
   trendingSelector,
   fetchTrendingProjects,
 } from "../../slices/trending.slice";
+import Search from "../Search/Search";
 import { CircularProgress } from "@mui/material";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import profile from "../../assets/profile.png";
 import "./Trending.css";
 import { notifyError } from "../../utils/notifyToasts";
-import { baseUrl } from "../../utils/constants";
 
 const Trending = () => {
   const history = useHistory();
   const dispatch = useDispatch();
-  const [focus, setFocus] = useState(false);
-  const [searchProfiles, setSearchProfiles] = useState([]);
   const { trendingProjects, trendingLoading, trendingError } =
     useSelector(trendingSelector);
 
@@ -29,90 +26,9 @@ const Trending = () => {
     notifyError("An error occurred");
   }
 
-  const search = async (query) => {
-    try {
-      const res = await fetch(`${baseUrl}/search?name=${query}`);
-      const data = await res.json();
-      if (data.success) {
-        setSearchProfiles(data.profilesFound);
-      } else {
-        notifyError("Failed to fetch search results");
-      }
-    } catch (err) {
-      notifyError("Failed to fetch search results");
-    }
-  };
-
-  let debounceTimeout = 0;
-
-  const handleSearch = async (e) => {
-    const query = e.target.value;
-    if (debounceTimeout) {
-      clearTimeout(debounceTimeout);
-    }
-
-    if (!query.length) {
-      setSearchProfiles([]);
-      return;
-    }
-
-    debounceTimeout = setTimeout(() => {
-      search(query);
-    }, 300);
-  };
-
   return (
     <div className="feed-right">
-      <input
-        type="text"
-        placeholder="Search developers"
-        className="search"
-        onFocus={() => setFocus(true)}
-        onBlur={() => setFocus(false)}
-        onChange={handleSearch}
-      />
-      {focus && (
-        <div className="search-results">
-          {searchProfiles.length > 0
-            ? searchProfiles.map((item) => {
-                return (
-                  <div
-                    to={`/profile/${item.username}`}
-                    className="follow-profile search-profile"
-                    key={item._id}
-                    onMouseDown={(e) => {
-                      e.preventDefault();
-                      history.push(`/profile/${item.username}`);
-                    }}
-                  >
-                    <br />
-                    &nbsp;&nbsp;
-                    <img
-                      src={
-                        "pic" in item
-                          ? item.pic
-                          : "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
-                      }
-                      alt=""
-                      className="follow-profile-img"
-                    />
-                    <span>{item.name}</span>
-                  </div>
-                );
-              })
-            : null}
-          {/* <Link to="/profile/sam" className="follow-profile search-profile">
-            <br />
-            &nbsp;&nbsp;
-            <img
-              src="https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
-              alt=""
-              className="follow-profile-img"
-            />
-            <span>Sam Doe</span>
-          </Link> */}
-        </div>
-      )}
+      <Search />
       {trendingLoading && <CircularProgress />}
       {trendingProjects.length && (
         <div className="trending">
